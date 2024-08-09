5454214dd104f46b7ee4d0edfc7fce7b88510074
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-09 06:44:06 -0300

    feat: alterações feitas para alternar entre texto para quando tem task adicionada e quando não tem, isso melhora a experiencia do usuário

--- app/src/components/molecules/PaginationControls.tsx
+++ app/src/components/molecules/PaginationControls.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Flex, Button, Text, Input, Select } from '@chakra-ui/react';
-import { SkipNext, SkipPrevious } from '../../../public/index'; // Ajuste conforme necessário
+import { SkipNext, SkipPrevious } from '../../../public/index';
 import usePaginationLogic from '../../hooks/usePagination';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/store';
 
 const PaginationControls: React.FC = () => {
   const {
@@ -19,36 +21,43 @@ const PaginationControls: React.FC = () => {
     totalPages,
   } = usePaginationLogic();
 
+  const dispatch = useDispatch();
+  const { filteredData } = useSelector((state: RootState) => ({
+    filteredData: state.register.filteredData,
+  }));
+
   return (
     <Flex direction="column" mt={4}>
       {/* Filtros na página */}
-      <Text textAlign='center' fontSize='27' p='6'>Filtrar Tasks</Text>
-      <Flex mb={4} direction="column" gap={2}>
-        <Input
-          value={localEditTitle}
-          onChange={(e) => handleChange('localEditTitle', e.target.value)}
-          mb={2}
-          placeholder="Title"
-        />
-        <Input
-          value={localEditDescription}
-          onChange={(e) => handleChange('localEditDescription', e.target.value)}
-          mb={2}
-          placeholder="Description"
-        />
-        <Select
-          placeholder="Select status"
-          value={statusFilter}
-          onChange={(e) => setStatusFilter(e.target.value)}
-        >
-          <option value="">Todas</option>
-          <option value="pending">Pendente</option>
-          <option value="completed">Concluída</option>
-        </Select>
-      </Flex>
+      <>
+        <Text textAlign='center' fontSize={filteredData.length < 1 ? '23' : '29'} p='6'>{filteredData.length < 1 ? 'Adicione uma tarefa e organize seu dia' : 'Filtrar Tasks'}</Text>
+        {filteredData.length > 0 && <Flex mb={4} direction="column" gap={2}>
+          <Input
+            value={localEditTitle}
+            onChange={(e) => handleChange('localEditTitle', e.target.value)}
+            mb={2}
+            placeholder="Title"
+          />
+          <Input
+            value={localEditDescription}
+            onChange={(e) => handleChange('localEditDescription', e.target.value)}
+            mb={2}
+            placeholder="Description"
+          />
+          <Select
+            placeholder="Select status"
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+          >
+            <option value="">Todas</option>
+            <option value="pending">Pendente</option>
+            <option value="completed">Concluída</option>
+          </Select>
+        </Flex>}
+      </>
 
       {/* Paginação */}
-      <Flex justify="space-between" align="center">
+      {filteredData.length > 0 && <Flex justify="space-between" align="center">
         <Button
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage < 2}
@@ -64,7 +73,7 @@ const PaginationControls: React.FC = () => {
         >
           <SkipNext />
         </Button>
-      </Flex>
+      </Flex>}
 
       {/* Exibição dos dados filtrados e paginados */}
       {localEditTitle || localEditDescription || statusFilter ? (
@@ -73,11 +82,11 @@ const PaginationControls: React.FC = () => {
           {paginatedData.length > 0 ? (
             <Flex direction="column" mt={4}>
               {paginatedData.map((item) => (
-                <Flex key={item.idTask} direction="column" p={2} borderWidth={1} mb={2}>
-                  <Text fontWeight="bold">{item.title}</Text>
-                  <Text>{item.description}</Text>
-                  <Text>Status: {item.status === 'completed' ? 'Concluída' : item.status === 'pending' ? 'Pendente' : ''}</Text>
-                </Flex>
+                <Flex key={item.idTask} direction="column" p={2} borderWidth={1} mb={2} color="#171b18">
+                <Text fontWeight="bold">Título: {item.title}</Text>
+                <Text>Descrição: {item.description}</Text>
+                <Text>Status: {item.status === 'completed' ? 'Concluída' : item.status === 'pending' ? 'Pendente' : ''}</Text>
+              </Flex>
               ))}
             </Flex>
           ) : (
